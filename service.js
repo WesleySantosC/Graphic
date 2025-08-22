89ab31e8e04c31484891d03990e3566aa9ad01d1
Wesley Santos Cardoso <155553388+WesleySantosC@users.noreply.github.com>
2025-08-22 19:47:19 -0300

    Add files via upload

---
+++ service.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    let names = ['Orange', 'Melon', 'strawberry', 'peach'];
+
+    let expenses = [
+        [12, 15, 20],
+        [10, 13, 15],
+        [18, 20, 25],
+        [5, 16, 18]
+    ];
+
+    let date = [1, 2, 3];
+
+    let seriesData = names.map((name, i) => {
+        return {
+            name: name,
+            data: expenses[i]
+        }
+    });
+
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line'
+        },
+        title: {
+            text: 'Valor'
+        },
+        tooltip: {
+            shared: true,
+            valuePrefix: 'R$ ' 
+        },
+        xAxis: {
+            categories: date
+        },
+        yAxis: {
+            title: { text: 'Valor' }
+        },
+        series: seriesData
+    });
+});
